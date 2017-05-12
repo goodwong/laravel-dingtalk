34cc88ddc7f89263decf009b6cad2bf282f7ff2e
william <goodwong@foxmail.com>
2017-05-12 07:51:03 +0000

    add DingtalkUser entity

---
+++ src/Entities/DingtalkUser.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Goodwong\LaravelDingtalk\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DingtalkUser extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'userid',
+        'name',
+        'remark',
+        'mobile',
+        'avatar',
+        'position',
+        'unionid',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'userid',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at',
+    ];
+}
