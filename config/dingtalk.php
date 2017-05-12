4c658f9c055395ba2e9b7970046afedad4714e28
william <goodwong@foxmail.com>
2017-05-12 07:52:20 +0000

    update config

--- config/dingtalk.php
+++ config/dingtalk.php
@@ -9,7 +9,7 @@ return [
     /**
      * 配置参数，在钉钉开发文档中查看
      */
-    'corp_id' => env('DINGTALK_CORP_ID', 'your-app-id'),
-    'corp_secret' => env('DINGTALK_CORP_SECRET', 'your-app-secret'),
-    'agent_id' => env('DINGTALK_AGENT_ID', 'your-app-secret'),
+    'corp_id' => env('DINGTALK_CORP_ID', 'your-corp-id'),
+    'corp_secret' => env('DINGTALK_CORP_SECRET', 'your-corp-secret'),
+    'agent_id' => env('DINGTALK_AGENT_ID', 'your-agent-id'),
 ];
