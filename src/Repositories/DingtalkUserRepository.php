a76b28e2c0085483334479a4f32925bdca1af4c5
william <goodwong@foxmail.com>
2017-05-12 07:51:21 +0000

    add repository

---
+++ src/Repositories/DingtalkUserRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Goodwong\LaravelDingtalk\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class DingtalkUserRepository extends BaseRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    function model()
+    {
+        return \Goodwong\LaravelDingtalk\Entities\DingtalkUser::class;
+    }
+
+    /**
+     * Retrieve first data of repository, or create new Entity
+     *
+     * @param array $attributes
+     * @param array $additions
+     * @return mixed
+     */
+    public function firstOrCreate(array $attributes = [], array $additions = [])
+    {
+        $this->applyCriteria();
+        $this->applyScope();
+        $temporarySkipPresenter = $this->skipPresenter;
+        $this->skipPresenter(true);
+        $model = $this->model->firstOrCreate($attributes, $additions);
+        $this->skipPresenter($temporarySkipPresenter);
+        $this->resetModel();
+        return $this->parserResult($model);
+    }
+}
